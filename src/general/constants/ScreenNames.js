5348e054d557f3894baa4855a7a7239a0f8bea48
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2023-03-12 13:40:47 +0700

    add folder structure, toDo screen

---
+++ src/general/constants/ScreenNames.js
@@ -0,0 +1,3 @@
+export const ScreenName={
+    toDoScreen: 'ToDoScreen',
+}
---
+++ src/modules/screens/ToDoScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles'
+import {View,Text,ImageBackground, SafeAreaView} from 'react-native'
+
+export default function TodoScreen(props){
+  return(
+    <View style={styles.container}><Text style={styles.text}>Hello World</Text></View>
+
+  )
+}
