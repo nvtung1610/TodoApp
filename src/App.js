d374760a61e3f789d50f3869fe0eed8cd56febf3
nvtungIT <tung.nv.work@gmail.com>
2023-04-22 16:10:34 +0700

    add error boundary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import AppNavigator from "./modules/navigators/AppNavigator";
-
-const App =()=>{
-  return(
-    <AppNavigator/>
-  )
-
-}
+import React from 'react';
+import AppNavigator from './modules/navigators/AppNavigator';
+import ErrorBoundary from './ErrorBoundary ';
+const App = () => {
+  return (
+    <ErrorBoundary fallback="There was an error">
+      <AppNavigator />
+    </ErrorBoundary>
+  );
+};
 
 export default App;
